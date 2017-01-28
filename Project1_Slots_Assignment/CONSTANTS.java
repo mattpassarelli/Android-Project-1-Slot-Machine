6936319ec32bdfede93e53215bba2dc243cbf121
Matt Passarelli <mattvpassarelli@gmail.com>
2017-01-28 18:40:37 -0500

    Added background and "GO" Button, and "dol" icon

---
+++ Project1_Slots_Assignment/CONSTANTS.java
@@ -0,0 +1,24 @@
+package YOUR_PACKAGENAME_HERE;
+
+/**
+ * Created by Perkins on 1/16/2016.
+ * cannot instantiate this class
+ * access staticly ex.
+ * int total_cash = CONSTANTS.STARTUP_CASH;
+ */
+public class CONSTANTS {
+
+    //dont allow instatiation
+    private CONSTANTS() {
+    }
+
+    public static final int NUMB_FLOWERS    = 3;
+    public static final int STARTUP_CASH    = 5;
+    public static final int YOUR_BROKE      = 0;
+    public static final int COST_PER_ROLL   = 1;
+    public static final int MATCH_2         = 2;
+    public static final int MATCH_3         = 3;
+
+
+
+}
