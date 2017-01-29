c68d9fa09d32a3bb1a83850a5250770725922c0d
Matt Passarelli <mattvpassarelli@gmail.com>
2017-01-28 23:14:57 -0500

    Finished Layout. Need to start the code and rotation XML

--- app/src/main/java/com/example/matt/project1slotmachine/MainActivity.java
+++ app/src/main/java/com/example/matt/project1slotmachine/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.matt.project1slotmachine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Button reset = (Button) findViewById(R.id.reset);
+        //reset.setVisibility(View.VISIBLE);
         setContentView(R.layout.activity_main);
+
     }
 }
