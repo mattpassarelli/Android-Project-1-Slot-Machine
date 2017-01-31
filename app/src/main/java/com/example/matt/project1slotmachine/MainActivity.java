29c814f7c832a8e2ca20df665170501a799b9df0
Matt Passarelli <mattvpassarelli@gmail.com>
2017-01-31 13:44:26 -0500

    Finished the app. All works well. Vibrates on button presses with toast for reset. Might continue improving it later on

--- app/src/main/java/com/example/matt/project1slotmachine/MainActivity.java
+++ app/src/main/java/com/example/matt/project1slotmachine/MainActivity.java
@@ -1,22 +1,163 @@
 package com.example.matt.project1slotmachine;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
-import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Vibrator;
 
-import static android.R.attr.button;
+import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Animation.AnimationListener {
+
+    private ImageView reset;
+    private ImageView flower1;
+    private ImageView flower2;
+    private ImageView flower3;
+    private TextView money;
+    private Animation rotate;
+    int amount;
+    Vibrator vibrate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //Button reset = (Button) findViewById(R.id.reset);
-        //reset.setVisibility(View.VISIBLE);
+
         setContentView(R.layout.activity_main);
+
+
+        reset = (ImageView) findViewById(R.id.reset);
+        flower1 = (ImageView) findViewById(R.id.flower1);
+        flower2 = (ImageView) findViewById(R.id.flower2);
+        flower3 = (ImageView) findViewById(R.id.flower3);
+        money = (TextView) findViewById(R.id.moneyCount);
+        rotate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
+        amount = 5;
+        vibrate = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        setMoney(amount);
+        rotate.setAnimationListener(this);
+    }
+
+    public void go(View view) {
+        flower1.setBackgroundResource(R.drawable.tmp);
+        flower2.setBackgroundResource(R.drawable.tmp);
+        flower3.setBackgroundResource(R.drawable.tmp);
+        flower1.startAnimation(rotate);
+        flower2.startAnimation(rotate);
+        flower3.startAnimation(rotate);
+        reset.setVisibility(View.VISIBLE);
+
+        vibrate.vibrate(100);
+
+        amount -= 1;
+        setMoney(amount);
+    }
+
+
+    private void setMoney(int amount) {
+        money.setText("$" + amount);
+    }
+
+    public void reset(View view) {
+        Toast.makeText(this, "Game has been reset", Toast.LENGTH_SHORT).show();
+
+        vibrate.vibrate(100);
+
+        amount = 5;
+        setMoney(amount);
+        flower1.setBackgroundResource(R.drawable.f1);
+        flower2.setBackgroundResource(R.drawable.f1);
+        flower3.setBackgroundResource(R.drawable.f1);
+
+        reset.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        changeFlowers();
+    }
+
+    private void changeFlowers() {
+        Random random = new Random();
+        int num1 = random.nextInt(3);
+        int num2 = random.nextInt(3);
+        int num3 = random.nextInt(3);
+
+        //Toast.makeText(this, "" + num1 + " " + num2 + " " + num3, Toast.LENGTH_SHORT).show();
+
+        int count = 0;
+
+        switch (num1) {
+            case 0:
+                flower1.setBackgroundResource(R.drawable.f1);
+                break;
+            case 1:
+                flower1.setBackgroundResource(R.drawable.f2);
+                break;
+            case 2:
+                flower1.setBackgroundResource(R.drawable.f3);
+                break;
+        }
+        switch (num2) {
+            case 0:
+                flower2.setBackgroundResource(R.drawable.f1);
+                break;
+            case 1:
+                flower2.setBackgroundResource(R.drawable.f2);
+                break;
+            case 2:
+                flower2.setBackgroundResource(R.drawable.f3);
+                break;
+        }
+        switch (num3) {
+            case 0:
+                flower3.setBackgroundResource(R.drawable.f1);
+                break;
+            case 1:
+                flower3.setBackgroundResource(R.drawable.f2);
+                break;
+            case 2:
+                flower3.setBackgroundResource(R.drawable.f3);
+                break;
+        }
+
+        if(num1 == num2)
+        {
+            count = 2;
+        }
+        else if(num2 == num3)
+        {
+            count = 2;
+        }
+        else if(num1 == num3)
+        {
+            count = 2;
+        }
+        else if(num1 == num2 && num2 == num3)
+        {
+            count = 3;
+        }
+
+       // Toast.makeText(this, ""+ count, Toast.LENGTH_LONG).show();
+        amount += count;
+        setMoney(amount);
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
 
     }
 }
+
